99e6f0505d72517d2cd2bfc9d32c39132d25c774
kacperkuta <kacperkuta@gmail.com>
2020-04-27 18:46:11 +0200

    2

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI, HTTPException
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.responses import RedirectResponse, Response
+
 app = FastAPI()
 
-from pydantic import BaseModel
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.post("/login")
+def redirect(username: str = Depends(get_current_username)):
+    response = RedirectResponse(url='/welcome')
+    return response
 
 @app.get("/")
 def root():
@@ -10,5 +33,5 @@ def root():
 
 @app.get("/welcome")
 def welcome_to_the_jungle():
-	return {"message": "Welcome to the jungle! We have funny games!"}
+	return {"message": "Hello World during the coronavirus pandemic!"}
 
